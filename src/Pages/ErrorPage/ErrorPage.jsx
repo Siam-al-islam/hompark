5660d31043fa0f351dedf578f05707e3a44c48f7
Siam-al-islam <siamalislam692@gmail.com>
2024-06-04 21:19:05 +0600

    error page added on UI

--- src/Pages/ErrorPage/ErrorPage.jsx
+++ src/Pages/ErrorPage/ErrorPage.jsx
@@ -1,7 +1,13 @@
-const ErrorPage = () => {
-    return (
-        <div>
+import { Link, useRouteError } from "react-router-dom";
 
+const ErrorPage = () => {
+    const error = useRouteError();
+
+    return (
+        <div className="text-center mt-32">
+            <h1 className="text-3xl">{error.status}</h1>
+            <p className="text-xl">{error.statusText}</p>
+            <Link to="/" className="btn mt-8 btn-wide bg-red-700 text-white">Back to Home</Link>
         </div>
     );
 };
