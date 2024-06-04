b9d22b009ccaa31c5aff25e21ab25f6fba4e0af2
Siam-al-islam <siamalislam692@gmail.com>
2024-06-04 18:21:36 +0600

    navbar added

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import Navbar from "./Shared/Navbar";
+
+const Home = () => {
+    return (
+        <div className="mt-8">
+            <Navbar />
+            <h2 className="text-3xl">This is Home</h2>
+        </div>
+    );
+};
+
+export default Home;
